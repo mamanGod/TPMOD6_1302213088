6e7bf21589aea2aa90b884f47349fe475762eed6
mamanGod <127042964+mamanGod@users.noreply.github.com>
2023-03-26 23:53:05 +0700

    menambahkan class SayaTubeVideo

---
+++ TPMOD6_1302213088/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Diagnostics.Contracts;
+
+public class SayaTubeVideo
+{
+    int id;
+    string title;
+    int playCount;
+
+    public SayaTubeVideo(string t)
+    {
+        this.title = t;
+        Random r = new Random();
+        id = r.Next(10000, 99999);
+        this.playCount = 0;
+    }
+
+    public void IncreasePlayCount(int pt)
+    {
+        this.playCount += pt;
+    }
+
+    public void PrintVideosDetails()
+    {
+        Console.WriteLine("id       : " + this.id);
+        Console.WriteLine("title    : " + this.title);
+        Console.WriteLine("playCount: " + this.playCount);
+    }
+
+    public static void Main(string[] args)
+    {
+        SayaTubeVideo video = new SayaTubeVideo("Tutorial Design By Contract - Rahman");
+        video.IncreasePlayCount(17);
+        video.PrintVideosDetails();
+    }
+}
